555e0f473f8425f939ad3ca2234d767d8c28a4a6
alfandialamshach <fandixpc1@gmail.com>
2024-03-25 10:45:30 +0700

    kesembilan

---
+++ script.js
@@ -0,0 +1,6 @@
+// script.js
+
+function deskripsi() {
+    var kotak = document.querySelector(".kotak"); // Pilih elemen dengan kelas "kotak"
+    kotak.style.display = "block"; // Tampilkan elemen "kotak"
+}
